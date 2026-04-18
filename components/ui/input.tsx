cb2e3e505ed740d9659a15b40b5ad64420254640
vzkiss <vzkiss@gmail.com>
2026-04-19 00:15:13 +0200

    update(input): placeholder color

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,7 +9,7 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
       type={type}
       data-slot="input"
       className={cn(
-        'h-9 w-full min-w-0 rounded-md border border-(--color-g60) bg-(--color-g80) px-2.5 py-1 text-base shadow-xs transition-[color,box-shadow,background-color,border-color] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-(--color-g10) focus-visible:bg-(--color-g60) focus-visible:ring-0 disabled:pointer-events-none disabled:cursor-not-allowed disabled:text-text-disabled aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm aria-invalid:focus-visible:border-destructive dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
+        'h-9 w-full min-w-0 rounded-md border border-(--color-g60) bg-(--color-g80) px-2.5 py-1 text-base shadow-xs transition-[color,box-shadow,background-color,border-color] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-text-disabled focus-visible:border-(--color-g10) focus-visible:bg-(--color-g60) focus-visible:ring-0 disabled:pointer-events-none disabled:cursor-not-allowed disabled:text-text-disabled aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm aria-invalid:focus-visible:border-destructive dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
         className
       )}
       {...props}
