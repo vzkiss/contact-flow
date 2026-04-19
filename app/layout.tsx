072c6a9bb31ef452af521cee52a8f84e3aa834e3
vzkiss <vzkiss@gmail.com>
2026-04-19 16:58:48 +0200

    add: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,25 @@ const lexendDeca = localFont({
   weight: '400',
 })
 
-export const metadata: Metadata = { title: 'Contact Flow' }
+export const metadata: Metadata = {
+  title: 'Contact Flow | vzkiss.com',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon.ico', type: 'image/x-icon' },
+      {
+        url: '/favicon/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+    ],
+    apple: '/favicon/apple-touch-icon.png',
+  },
+}
 
 export const viewport: Viewport = {
   colorScheme: 'dark',
